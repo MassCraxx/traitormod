b5f1f8d4c0a1a913669c30b2d0c1a2fb07abc677
MassCraxx <masscraxx@users.noreply.github.com>
2022-05-30 01:35:30 +0200

    version RC6

--- Lua/traitormod.lua
+++ Lua/traitormod.lua
@@ -1,4 +1,4 @@
-Traitormod.VERSION = "2.1-RC5"
+Traitormod.VERSION = "2.1-RC6"
 
 print("Traitor Mod v" .. Traitormod.VERSION .. " by Evil Factory")
 print("Special thanks to Qunk, Femboy69 and JoneK for helping in the development of this mod.")
